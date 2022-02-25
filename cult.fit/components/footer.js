d1f5fc7c2b08213578e688fd34b92f562c72ef48
chaitanya8080 <96075581+chaitanya8080@users.noreply.github.com>
2022-02-26 01:47:23 +0530

    Add files via upload

---
+++ cult.fit/components/footer.js
@@ -0,0 +1,38 @@
+
+
+function footer(){
+    return `  <div id="logo_info">
+    <div id="logo">
+    <a href="index.html"><img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_120,ar_3.87,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/vman-and-white-cult-text.png" alt=""></a>
+</div>
+<div id="footer_text">
+    <p>At cult.fit, we make group workout fun ,daily food
+        healthy and tasty,mental fitness easy with yoga and
+        meditation,Medical and lifestyle care hussle-free
+        #beBetterEveryday</p>
+</div>
+
+</div>
+<div id="terms">
+    <div id="conditions">
+        <div><p>cult.fit business</p><p>partner.fit</p><p>contact us</p></div>
+        <div><p>cult.fit franchise</p><p>blogs</p><p>privacy policy</p></div>
+        <div><p>carporate partnership</p><p>talks</p><p>refund policy</p></div>
+        <div><p>cult pass network</p><p>careers</p><p>terms & conditions</p></div>
+    </div>
+
+</div>
+
+<div id="company">
+    <div><img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,q_auto:eco,dpr_2,f_auto,fl_progressive//image/footer-web/FooterV3/app-store.png" alt=""></div>
+    <div><img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,q_auto:eco,dpr_2,f_auto,fl_progressive//image/footer-web/FooterV3/google-play.png" alt=""></div>
+    <div id="fb_image"><img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,q_auto:eco,dpr_2,f_auto,fl_progressive//image/footer-web/FooterV3/youtube.svg" alt="">
+    <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,q_auto:eco,dpr_2,f_auto,fl_progressive//image/footer-web/FooterV3/fb-page.svg" alt="">
+    <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,q_auto:eco,dpr_2,f_auto,fl_progressive//image/footer-web/FooterV3/twitter-page.svg" alt="">
+    <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,q_auto:eco,dpr_2,f_auto,fl_progressive//image/footer-web/FooterV3/insta-page.svg" alt="">
+
+    </div>
+</div>
+`
+}
+export default footer;
---
+++ cult.fit/components/navbar.js
@@ -0,0 +1,20 @@
+
+function navbar(){
+  return `   
+
+  <div id="logo_div"> <a href="index.html"><img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_120,ar_3.87,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/vman-and-white-cult-text.png" alt="" id="logo_img"></a></div>
+  <div id="types_div">
+      <P> <a href="fitness.html">FITNESS</a></P>
+      <P> <a href="care.html">CARE</a></P>
+      <!-- <P>EAT</P> -->
+      <P>MIND</P>
+      <P> <a href="store.html">STORE</a></P>
+  </div>
+ <div id="cart_div">
+  <P ><img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_26,ar_1,q_auto:eco,dpr_2,f_auto,fl_progressive/image/test/header/location.png" alt="" id="place"></P>
+  <P ><button id="button1">GET APP</button></P>
+  <P><a href="signup.html"><img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_26,ar_1,q_auto:eco,dpr_2,f_auto,fl_progressive/image/test/header/Profile.png" alt="" id="place"></a></P>
+  <p><a href="display.html"><img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/cart-dark-theme.svg" alt="" id="cart_block"></a> </P>
+ </div> `
+}
+export default navbar;
